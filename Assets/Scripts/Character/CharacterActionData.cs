d158e3489add833c0f90ba54c258ac65d5eaf2e4
andongni <x102039@gmail.com>
2024-03-09 19:55:01 +0800

    v1.1 - fix bug about enemy state /  laser enemy out of the screen

---
+++ Assets/Scripts/Character/CharacterActionData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterActionData : MonoBehaviour
+{
+    //[Header("Component")]
+    //[Header("Settings")]
+    //[Header("Debug")]
+}
